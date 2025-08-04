8588ba1489c7f68f9f22f7aaefe2d86656347c23
Sham <yong178344@gmail.com>
2025-08-04 18:51:22 +0800

    ...

--- screens/LifestyleDataInputScreen.js
+++ screens/LifestyleDataInputScreen.js
@@ -327,7 +327,7 @@ const LifestyleDataInputScreen = () => {
       await AsyncStorage.setItem("userProfileData", JSON.stringify(data));
 
       // Make POST request to FastAPI
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("http://192.168.0.107:8000/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
